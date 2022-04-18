e9cf9b1a3abee5a9d3548a4c59d7fc34b7278d79
Your Name <you@example.com>
2022-04-18 10:10:45 -0400

    1st commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
 const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hellio Rashhhh'));
+app.get('/', (req, res) => res.send('wabisabi.png'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
+
