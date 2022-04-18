c64ba72355e51a5ccec7224a6f3db7ade9f49097
Your Name <you@example.com>
2022-04-18 10:12:40 -0400

    2nd commut

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('wabisabi.png'));
+app.get('/', (req, res) => res.send('My picture'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
